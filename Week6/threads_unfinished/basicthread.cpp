5d6d8e4826c9c92f7ad4a4258bb4cb6ca9602911
colinauyeung <daisuke.moto403@gmail.com>
2022-10-18 16:20:09 -0600

    Test

--- Week6/threads_unfinished/basicthread.cpp
+++ Week6/threads_unfinished/basicthread.cpp
@@ -3,38 +3,7 @@
 #include <string> 
 #include <unistd.h>   
 
-struct input{
-    int id;
-    char name;
-};
-
-void * threadcode(void * arg){
-    struct input * in = (struct input *) arg;
-    int id = in->id;
-    char name = in->name;
-    printf("Hello I'm thead %d, my name is %c\n", id, name);
-    pthread_exit(NULL);
-    return NULL;
-}
-
 int main(){
-    int nthread = 4;
-    pthread_t pool[nthread];
-    struct input in[nthread];
 
-    for(int i = 0; i<nthread; i++){
-        in[i].id = i;
-        in[i].name = 'a' + i; 
-    }
-
-    for(int i = 0; i<nthread; i++){
-        pthread_create(&pool[i], NULL, threadcode, &in[i]);
-    }
-
-    for(int i = 0; i<nthread; i++){
-        pthread_join(pool[i], NULL);
-    }
-
-    return 0;
 
 }
